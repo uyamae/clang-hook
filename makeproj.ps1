9886026be75ada7befed9a482eb8246dc9800a78
uyamae <uyamae@gmail.com>
2023-08-09 09:04:54 +0900

    初回のコミット

---
+++ makeproj.ps1
@@ -0,0 +1,13 @@
+Set-Location -Path $PSScriptRoot
+# make build directory exists
+$BuildDir = "build"
+if (!(Test-Path $BuildDir)) {
+    New-Item $BuildDir -ItemType Directory
+}
+# move into build directory
+if (!(Push-Location -Path $BuildDir -PassThru)) {
+    Write-Host "${BuildDir} is not a directory."
+    Exit 1
+}
+# execute cmake
+cmake .. -G"Visual Studio 17 2022"
